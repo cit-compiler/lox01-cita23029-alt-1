703b31de1994190b26b8eba58990c09afe3c2a2a
cita23029 <cita23029@g.nihon-u.ac.jp>
2025-12-09 11:56:57 +0900

    add AstPrinter.java

--- codes/com/craftinginterpreters/lox/AstPrinter.java
+++ codes/com/craftinginterpreters/lox/AstPrinter.java
@@ -224,6 +224,17 @@ class AstPrinter implements Expr.Visitor<String>, Stmt.Visitor<String> {
     return builder.toString();
   }
 //< print-utilities
+public static void main(String[] args) {
+    Expr expression = new Expr.Binary(
+        new Expr.Unary(
+            new Token(TokenType.MINUS, "-", null, 1),
+            new Expr.Literal(123)),
+        new Token(TokenType.STAR, "*", null, 1),
+        new Expr.Grouping(
+            new Expr.Literal(45.67)));
+
+    System.out.println(new AstPrinter().print(expression));
+  }
 //> omit
   // Note: AstPrinting other types of syntax trees is not shown in the
   // book, but this is provided here as a reference for those reading
