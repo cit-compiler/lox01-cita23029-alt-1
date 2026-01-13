5e449c0e9ed2c3d4675ad1b0b40ef0f359ab506b
cita23029 <cita23029@g.nihon-u.ac.jp>
2026-01-13 12:38:28 +0900

    add

--- codes/com/craftinginterpreters/lox/Environment.java
+++ codes/com/craftinginterpreters/lox/Environment.java
@@ -1,15 +1,42 @@
-//> Statements and State environment-class
 package com.craftinginterpreters.lox;
 
 import java.util.HashMap;
 import java.util.Map;
 
 class Environment {
-//> enclosing-field
   final Environment enclosing;
-//< enclosing-field
   private final Map<String, Object> values = new HashMap<>();
-//> environment-constructors
+
+  Object get(Token name) {
+    if (values.containsKey(name.lexeme)) {
+      return values.get(name.lexeme);
+    }
+
+    if (enclosing != null) return enclosing.get(name);
+
+    throw new RuntimeError(name,
+        "Undefined variable '" + name.lexeme + "'.");
+  }
+
+  void assign(Token name, Object value) {
+    if (values.containsKey(name.lexeme)) {
+      values.put(name.lexeme, value);
+      return;
+    }
+
+    if (enclosing != null) {
+      enclosing.assign(name, value);
+      return;
+    }
+    
+    throw new RuntimeError(name,
+        "Undefined variable '" + name.lexeme + "'.");
+  }
+
+  void define(String name, Object value) {
+    values.put(name, value);
+  }
+
   Environment() {
     enclosing = null;
   }
@@ -17,75 +44,4 @@ class Environment {
   Environment(Environment enclosing) {
     this.enclosing = enclosing;
   }
-//< environment-constructors
-//> environment-get
-
-  Object get(Token name) {
-    if (values.containsKey(name.lexeme)) {
-      return values.get(name.lexeme);
-    }
-//> environment-get-enclosing
-
-    if (enclosing != null) return enclosing.get(name);
-//< environment-get-enclosing
-
-    throw new RuntimeError(name,
-        "Undefined variable '" + name.lexeme + "'.");
-  }
-
-//< environment-get
-//> environment-assign
-  void assign(Token name, Object value) {
-    if (values.containsKey(name.lexeme)) {
-      values.put(name.lexeme, value);
-      return;
-    }
-
-//> environment-assign-enclosing
-    if (enclosing != null) {
-      enclosing.assign(name, value);
-      return;
-    }
-
-//< environment-assign-enclosing
-    throw new RuntimeError(name,
-        "Undefined variable '" + name.lexeme + "'.");
-  }
-//< environment-assign
-//> environment-define
-  void define(String name, Object value) {
-    values.put(name, value);
-  }
-//< environment-define
-//> Resolving and Binding ancestor
-  Environment ancestor(int distance) {
-    Environment environment = this;
-    for (int i = 0; i < distance; i++) {
-      environment = environment.enclosing; // [coupled]
-    }
-
-    return environment;
-  }
-//< Resolving and Binding ancestor
-//> Resolving and Binding get-at
-  Object getAt(int distance, String name) {
-    return ancestor(distance).values.get(name);
-  }
-//< Resolving and Binding get-at
-//> Resolving and Binding assign-at
-  void assignAt(int distance, Token name, Object value) {
-    ancestor(distance).values.put(name.lexeme, value);
-  }
-//< Resolving and Binding assign-at
-//> omit
-  @Override
-  public String toString() {
-    String result = values.toString();
-    if (enclosing != null) {
-      result += " -> " + enclosing.toString();
-    }
-
-    return result;
-  }
-//< omit
 }
